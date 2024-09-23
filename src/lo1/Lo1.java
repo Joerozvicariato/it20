00794f557733303d62e6802d1c8796c6303eb9a9
Joerozvicariato <joerozvicariato287@gmail.com>
2024-09-23 21:27:14 +0800

    Prime number

--- src/lo1/Lo1.java
+++ src/lo1/Lo1.java
@@ -1,12 +1,33 @@
 
 package lo1;
-
+import java.util.Scanner;
 public class Lo1 {
 
     public static void main(String[] args) {
    
-   System.out.println("Hello World");
+     Scanner scanner = new Scanner(System.in);
         
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        
+        if (isPrime(number)) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+        
+        scanner.close();
     }
-    
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false; 
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false; 
+            }
+        }
+        return true;
+    }
 }
