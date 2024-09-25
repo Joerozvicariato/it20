39150de3f3b9ab1e88c548e43d553e778e134366
joerozvicariato287@gmail.om <Joerozvicariato>
2024-09-25 09:52:53 +0800

    Added metrix

---
+++ src/Multidimensional_Array/Multidimensional_Array.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Multidimensional_Array;
+
+/**
+ *
+ * @author asus
+ */
+public class Multidimensional_Array {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int[][] matrix = {
+	 {1, 2, 3},
+         {4, 5, 6},
+         {7, 8, 9}
+};
+    }
+    
+}
