b2e2712fd69b6156a73cfe9be26f626804b09a74
joerozvicariato287@gmail.om <Joerozvicariato>
2024-09-25 09:47:46 +0800

    Added String array

---
+++ src/Display_via_for_loop/Display_via_for_loop.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Display_via_for_loop;
+
+/**
+ *
+ * @author asus
+ */
+public class Display_via_for_loop {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        String[] arrayName = {"Value 1", "Value 2", "Value 3"};
+
+    }
+}
