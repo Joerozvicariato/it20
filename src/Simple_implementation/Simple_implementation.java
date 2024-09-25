9ffbf460ea46b92674b075486e29f2b57f30966b
joerozvicariato287@gmail.om <Joerozvicariato>
2024-09-25 09:28:07 +0800

    Added array

---
+++ src/Simple_implementation/Simple_implementation.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Simple_implementation;
+
+/**
+ *
+ * @author asus
+ */
+public class Simple_implementation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     
+       //Via index
+     String[] arrayName = {"Value 1","Value 2", "Value 3"};
+     
+     //Via Loop
+     for(int i=0; i< arrayName .length; i++) {
+     System.out.println(arrayName [i]);	
+   
+    }
+    }
+}
