724609664d3e81b3c25d537b16eaba27de3285a1
joerozvicariato287@gmail.om <Joerozvicariato>
2024-09-25 09:54:13 +0800

    Added 2D Array

--- src/Multidimensional_Array/Multidimensional_Array.java
+++ src/Multidimensional_Array/Multidimensional_Array.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package Multidimensional_Array;
 
 /**
@@ -16,11 +15,19 @@ public class Multidimensional_Array {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       int[][] matrix = {
-	 {1, 2, 3},
-         {4, 5, 6},
-         {7, 8, 9}
-};
+        int[][] matrix = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+       // Printing the 2D array
+        System.out.println("Matrix:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println(); // Move to the next line after printing each row
+        }
     }
-    
+
 }
