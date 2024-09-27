569afaa9d8bf474fe03c86097cda80c427b39bdc
Joerozvicariato <joerozvicariato287@gmail.com>
2024-09-27 20:50:04 +0800

    Added the Display info

---
+++ src/arraylist/ItemManager.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arraylist;
+
+/**
+ *
+ * @author Joeroz
+ */
+public class ItemManager {
+
+  
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/arraylist/main.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package arraylist;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Joeroz
+ */
+public class main {
+ private static ArrayList<String> items = new ArrayList<>();
+    private static Scanner scanner = new Scanner(System.in);
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) {
+       boolean running = true;
+        while (running) {
+            System.out.println("\nItem Manager");
+            System.out.println("1. Display items");
+            System.out.println("2. Insert item");
+            System.out.println("3. Update item");
+            System.out.println("4. Find item");
+            System.out.println("5. Delete item");
+            System.out.println("6. Exit");
+            System.out.print("Enter your choice: ");
+
+        }    
+    }
+}
