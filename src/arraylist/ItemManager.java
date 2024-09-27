356ab7515192b8fc1360fd16e3a23b91921d5996
Joerozvicariato <joerozvicariato287@gmail.com>
2024-09-28 06:07:54 +0800

    Added main and Item manager classes in arraylist package

--- src/arraylist/ItemManager.java
+++ src/arraylist/ItemManager.java
@@ -3,16 +3,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package arraylist;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 
 /**
  *
  * @author Joeroz
  */
 public class ItemManager {
-
-  
+private static ArrayList<String> items = new ArrayList<>();
+  private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         
     }
-    
 }
--- src/arraylist/main.java
+++ src/arraylist/main.java
@@ -3,7 +3,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package arraylist;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -11,7 +10,6 @@ import java.util.Scanner;
  * @author Joeroz
  */
 public class main {
- private static ArrayList<String> items = new ArrayList<>();
     private static Scanner scanner = new Scanner(System.in);
 
     /**
@@ -19,17 +17,7 @@ public class main {
      */
     
     public static void main(String[] args) {
-       boolean running = true;
-        while (running) {
-            System.out.println("\nItem Manager");
-            System.out.println("1. Display items");
-            System.out.println("2. Insert item");
-            System.out.println("3. Update item");
-            System.out.println("4. Find item");
-            System.out.println("5. Delete item");
-            System.out.println("6. Exit");
-            System.out.print("Enter your choice: ");
-
-        }    
+     Scanner scanner = new Scanner(System.in);
+        
+        }
     }
-}
