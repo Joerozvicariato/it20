c9284cbf619ff5f3d6a128283a5d0d6c85f29f3f
joerozvicariato287@gmail.om <Joerozvicariato>
2024-10-04 09:21:43 +0800

    Added Scanner

--- src/topic_2_linkedlist_string/Main.java
+++ src/topic_2_linkedlist_string/Main.java
@@ -16,40 +16,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       LinkedList list = new LinkedList();
+         LinkedList list = new LinkedList();
         Scanner scanner = new Scanner(System.in);
-         
-        while (true) {
-            System.out.println("\n1. Add node");
-            System.out.println("2. Remove node");
-            System.out.println("3. Display list");
-            System.out.println("4. Exit");
-            System.out.print("Enter your choice: ");
-            
-            int choice = scanner.nextInt();
-            scanner.nextLine(); // Consume newline
-            
-            switch (choice) {
-                case 1:
-                    System.out.print("Enter data to add: ");
-                    String data = scanner.nextLine();
-                    list.addNode(data);
-                    break;
-                case 2:
-                    System.out.print("Enter data to remove: ");
-                    data = scanner.nextLine();
-                    list.removeNode(data);
-                    break;
-                case 3:
-                    list.displayList();
-                    break;
-                case 4:
-                    System.out.println("Exiting...");
-                    scanner.close();
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-        }
     }
 }
