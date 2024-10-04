b061c5e39d14f98d62d4e516147ab7858b9d9ef6
joerozvicariato287@gmail.om <Joerozvicariato>
2024-10-04 09:19:00 +0800

    Added method for adding moving and swapping nodes

--- src/topic_2_linkedlist_int/LinkedList.java
+++ src/topic_2_linkedlist_int/LinkedList.java
@@ -1,17 +1,22 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package topic_2_linkedlist_int;
 
-/**
- *
- * @author asus
- */
+
 public class LinkedList {
-     private Node head; 
+    private Node head;
+
+    // Node class representing each element in the linked list
+    private static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
     // Method to add a new node at the end of the list
     public void add(int data) {
         Node newNode = new Node(data);
@@ -23,10 +28,11 @@ public class LinkedList {
         while (current.next != null) {
             current = current.next;
         }
-    
+
         current.next = newNode;
-    } 
-        // Method to print the linked list
+    }
+
+    // Method to print the linked list
     public void printList() {
         Node current = head;
         while (current != null) {
@@ -35,69 +41,34 @@ public class LinkedList {
         }
         System.out.println("null");
     }
-    
+
     // Method to delete a node by value
     public void deleteByValue(int value) {
         if (head == null) return;
+
         // If the head needs to be removed
         if (head.data == value) {
             head = head.next;
             return;
         }
+
         Node current = head;
         while (current.next != null) {
             if (current.next.data == value) {
-                current.next = current.next.next; 
+                current.next = current.next.next;
                 return;
             }
             current = current.next;
         }
     }
-    
-    
-    
-  public void moveNodePointer(int currentIndex, int newIndex) {
+
+    // Method to move a node from one position to another
+    public void moveNodePointer(int currentIndex, int newIndex) {
         if (head == null || currentIndex == newIndex) return;
-        
+
         Node current = head;
         Node prev = null;
         Node movingNode = null;
         Node movingPrev = null;
         // Find the node to move
-        for (int i = 0; current != null && i < currentIndex; i++) {
-            movingPrev = prev;
-            prev = current;
-            current = current.next;
-        }
-        movingNode = current;
-        // If the node to move was not found
-        if (movingNode == null) return;
-        // Remove the node from its current position
-        if (movingPrev != null) {
-            movingPrev.next = movingNode.next;
-        } else {
-        	// Moving the head
-            head = movingNode.next; 
-        }
-        // Insert the node at the new position
-        current = head;
-        prev = null;
-        for (int i = 0; current != null && i < newIndex; i++) {
-            prev = current;
-            current = current.next;
-        }
-        if (prev != null) {
-            movingNode.next = current;
-            prev.next = movingNode;
-        } else {
-            movingNode.next = head;
-            head = movingNode;
-        }
-   
-    }
-
-}
-    
-    
-
-
+        for (int i = 0; current !=
--- src/topic_2_linkedlist_string/Main.java
+++ src/topic_2_linkedlist_string/Main.java
@@ -5,7 +5,7 @@
  */
 
 package topic_2_linkedlist_string;
-
+import java.util.Scanner;
 /**
  *
  * @author asus
@@ -16,7 +16,40 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       LinkedList list = new LinkedList();
+        Scanner scanner = new Scanner(System.in);
+         
+        while (true) {
+            System.out.println("\n1. Add node");
+            System.out.println("2. Remove node");
+            System.out.println("3. Display list");
+            System.out.println("4. Exit");
+            System.out.print("Enter your choice: ");
+            
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+            
+            switch (choice) {
+                case 1:
+                    System.out.print("Enter data to add: ");
+                    String data = scanner.nextLine();
+                    list.addNode(data);
+                    break;
+                case 2:
+                    System.out.print("Enter data to remove: ");
+                    data = scanner.nextLine();
+                    list.removeNode(data);
+                    break;
+                case 3:
+                    list.displayList();
+                    break;
+                case 4:
+                    System.out.println("Exiting...");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
     }
-    
 }
