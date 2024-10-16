0dacec0312d52c6592362d69349739540523285d
joerozvicariato287@gmail.om <joerozvicariato287@gmail.com>
2024-10-16 22:10:58 +0800

    Added switch case on my main class

--- src/topic_4_queue/Main.java
+++ src/topic_4_queue/Main.java
@@ -3,14 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package topic_4_queue;
 
+package topic_4_queue;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
 
 import java.util.Scanner;
-
 /**
  *
  * @author joeroz
@@ -25,5 +24,37 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String command;
 
+        System.out.println("Welcome to the Customer joeroz System!");
+        do {
+            System.out.print("Enter command (add, serve, display, exit): ");
+            command = scanner.nextLine();
+
+            switch (command.toLowerCase()) {
+                case "add":
+                    System.out.print("Enter customer name: ");
+                    String name = scanner.nextLine();
+                    queue.add(new Customer(name)); // Enqueue customer
+                    System.out.println("Customer " + name + " added to the queue.");
+                    break;
+                case "serve":
+                    Customer servedCustomer = queue.poll(); // Dequeue customer
+                    if (servedCustomer != null) {
+                        System.out.println("Serving customer: " + servedCustomer.getName());
+                    } else {
+                        System.out.println("No customers in the queue.");
+                    }
+                    break;
+                case "display":
+                    System.out.println("Current queue: " + queue);
+                    break;
+                case "exit":
+                    System.out.println("Exiting the system.");
+                    break;
+                default:
+                    System.out.println("Invalid command. Please try again.");
+            }
+        } while (!command.equalsIgnoreCase("exit"));
+
+        scanner.close();
     }
 }
