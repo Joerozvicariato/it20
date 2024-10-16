daed91c081ca40a00ced7d062572022d5effede1
hiether17 <lhietherfhiona@gmail.com>
2024-10-17 00:32:42 +0800

    added display area

--- src/Topic_3_StackCalculator/Calculator.java
+++ src/Topic_3_StackCalculator/Calculator.java
@@ -28,7 +28,15 @@ public class StackCalculator extends JFrame implements ActionListener {
         setLayout(new BorderLayout(10, 10));
         setResizable(false);
     }
-
+      // Setup the display area
+    private void setupDisplay() {
+        display = new JTextArea(3, 20);
+        display.setEditable(false);
+        display.setFont(new Font("Arial", Font.PLAIN, 16));
+        display.setMargin(new Insets(10, 10, 10, 10));
+        JScrollPane scrollPane = new JScrollPane(display);
+        add(scrollPane, BorderLayout.NORTH);
+    }
         @Override
         public void actionPerformed(ActionEvent ae) {
             throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
