da7ca6a24e113aad2bf700f9afcce05536951ec3
Joerozvicariato <joerozvicariato287@gmail.com>
2024-11-28 23:56:50 +0800

    Added logic and recursive quicksort calls for sorting subarrays

--- src/topic_6_sorting/QuickSort.java
+++ src/topic_6_sorting/QuickSort.java
@@ -5,8 +5,14 @@ import java.util.Scanner;
 
 public class QuickSort {
 
-    
     public static void quickSort(ArrayList<Integer> arr, int low, int high) {
-        
+        if (low < high) {
+            int pi = partition(arr, low, high);
+
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
     }
+
 }
+
