6da77cd0b4eba2955b5be562d4d7563a6f349188
Joerozvicariato <joerozvicariato287@gmail.com>
2024-11-29 00:29:53 +0800

    Added loop to find the minimum element in the unsorted portion for selection port

--- src/topic_6_sorting/SelectionSort.java
+++ src/topic_6_sorting/SelectionSort.java
@@ -6,6 +6,14 @@ import java.util.Scanner;
 public class SelectionSort {
 
     public static void selectionSort(ArrayList<Integer> arr) {
-    
+        int n = arr.size();
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr.get(j) < arr.get(minIndex)) {
+                    minIndex = j;
+                }
+            }
+        }
     }
 }
