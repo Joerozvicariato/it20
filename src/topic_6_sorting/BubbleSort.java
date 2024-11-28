fd1aa3f64dc6a463fb79c07f7c343b87609b4635
Joerozvicariato <joerozvicariato287@gmail.com>
2024-11-28 22:03:44 +0800

    Added nested loops and comparison logic for bubblesort

--- src/topic_6_sorting/BubbleSort.java
+++ src/topic_6_sorting/BubbleSort.java
@@ -10,5 +10,9 @@ public class BubbleSort {
  
     public static void bubbleSort(ArrayList<Integer> arr) {
         int n = arr.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+            }
+        }
     }
 }
