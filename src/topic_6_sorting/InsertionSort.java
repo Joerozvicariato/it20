20f1f4208c552c41be2b39df904d8c1acf7771fb
Joerozvicariato <joerozvicariato287@gmail.com>
2024-11-28 22:41:05 +0800

    iterate throuh the arraylist and prepare for insertion eith key and index variables

--- src/topic_6_sorting/InsertionSort.java
+++ src/topic_6_sorting/InsertionSort.java
@@ -7,6 +7,10 @@ public class InsertionSort {
 
     public static void insertionSort(ArrayList<Integer> arr) {
         int n = arr.size();
+         for (int i = 1; i < n; i++) {
+            int key = arr.get(i); 
+            int j = i - 1;
 
+         }
     }
 }
